b50d187a282a4f7c281b7e1f48ccfcad7a4fdfdb
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-05 14:09:12 +0530

    Created basic layout of skills section

---
+++ src/sections/Skills.tsx
@@ -0,0 +1,16 @@
+import { FaSquareJs,FaPython,FaReact} from "react-icons/fa6"
+import { TbBrandHtml5, TbBrandCss3,TbBrandNodejs } from "react-icons/tb";
+import { BiLogoMongodb } from "react-icons/bi";
+import { SiExpress } from "react-icons/si";
+
+const Skills = () => {
+    return (
+        <div className='bg-[#050616] w-full h-full xl:w-4/5 xl:h-4/5 flex flex-col justify-around py-2 items-center lg:shadow-[0px_0px_56px_5px_rgba(255,255,255,0.25)] lg:rounded-4xl'>
+            <h1 className='w-max text-4xl text-center font-bold bg-linear-45/shorter from-indigo-500 from-5% via-cyan-400 via-20%% to-emerald-300  bg-clip-text text-transparent'>Skills</h1>
+
+            
+        </div>
+    )
+}
+
+export default Skills
