288f1db742c263ee1ec924905226d138c0134bf0
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-05 14:22:19 +0530

    Added section to the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import About from "./sections/About"
+import Education from "./sections/Education"
+import Navbar from "./sections/Navbar"
+import Skills from "./sections/Skills"
 
 function App() {
+
   return (
     <>
-      <div className='w-screen h-screen bg-black'>
+      <div className='relative w-screen h-screen scroll-smooth bg-[#050816] text-white overflow-x-none overflow-y-auto snap-y snap-mandatory *:snap-center [&::-webkit-scrollbar]:hidden'>
+        <Navbar />
+        <section id="about" className="w-screen h-screen flex flex-col items-center justify-center">
+          <About />
+        </section>
+        <section id="skills" className="w-screen h-screen flex items-center justify-center">
+          <Skills />
+        </section>
+        <section id="education" className="w-screen h-screen flex items-center justify-center">
+          <Education />
+        </section>
         
+
       </div>
     </>
   )
