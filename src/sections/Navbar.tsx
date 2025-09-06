f65e944542f24ac90b0f32a3ea52b56ab3be87ec
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-06 11:56:48 +0530

    Added navigation to projects section

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { TiThMenu } from 'react-icons/ti'
 import { LuBrainCircuit } from 'react-icons/lu'
 import { IoClose, IoPerson, IoSchool } from 'react-icons/io5'
+import { GoGoal } from "react-icons/go";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div className='fixed w-64 h-64 flex items-center rounded-l-full *:transition-all top-[calc(50%-8rem)] -right-32 z-50'>
             <button
-                className='absolute right-1/2 p-3 rounded-l-full shadow-[0px_0px_4px_4px_rgba(255,255,255,0.25)]'
+                className='absolute right-1/2 p-3 rounded-l-full shadow-[0px_0px_4px_4px_rgba(255,255,255,0.25)] hover:scale-100'
                 onClick={() => setIsOpen(!isOpen)}>
                 {isOpen ? <IoClose /> : <TiThMenu />}
             </button>
@@ -21,9 +22,9 @@ const Navbar = () => {
             <a href='#education' title='Education' onClick={() => setIsOpen(false)} className={`delay-50  ${isOpen ? 'rotate-15 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
                 <IoSchool className='-rotate-15' />
             </a>
-            <button onClick={() => setIsOpen(false)} className={`delay-75 ${isOpen ? '-rotate-15 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
-                <LuBrainCircuit />
-            </button>
+            <a  href='#projects' title='Projects' onClick={() => setIsOpen(false)} className={`delay-75 ${isOpen ? '-rotate-15 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
+                <GoGoal className='rotate-15'/>
+            </a>
             <button onClick={() => setIsOpen(false)} className={`delay-100 ${isOpen ? '-rotate-45 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
                 <LuBrainCircuit />
             </button>
