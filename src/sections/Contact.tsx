b2ef257831cd83b4804bcbcff7c7514f383fb1ad
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-08 18:57:42 +0530

    Added messaging to contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,19 +5,33 @@ const Contact = () => {
     return (
         <div className='bg-[#050616] w-full h-full xl:w-4/5 xl:h-4/5 flex flex-col justify-around pt-4 items-center lg:rounded-4xl lg:shadow-[0px_0px_56px_5px_rgba(255,255,255,0.25)]'>
             <h1 className='w-max text-4xl text-center font-bold bg-linear-45/shorter from-indigo-500 from-5% via-cyan-400 via-20%% to-emerald-300  bg-clip-text text-transparent'>Contact</h1>
-            <div className='w-9/10 h-9/10 flex flex-col justify-around items-center overflow-y-scroll [&::-webkit-scrollbar]:hidden'>
-                <div className='flex flex-col md:flex-row w-1/2 h-24 items-center justify-center gap-x-3 md:gap-x-6 hover:[&>a]:underline'>
-                    <VscGithub className='w-24 h-full transition-colors'/>
-                    <a className='text-white text-2xl md:text-4xl' target='_blank' href='https://github.com/ajit202006'>My Github</a>
+            <div className='w-full h-9/10 flex flex-col gap-y-7 md:flex-row justify-around items-center overflow-y-auto [&::-webkit-scrollbar]:hidden'>
+                <div className='w-4/5 md:w-2/5 h-full flex flex-col items-center justify-center gap-7 *:flex *:flex-col md:*:flex-row *:items-center *:gap-2 *:text-xl md:*:text-2xl *:hover:*:underline'>
+                    <p>You can see my work and connect with me on the following patterns :</p>
+                    <a href='https://github.com/ajit202006/' target='_blank'>
+                        <VscGithub className='text-3xl' />
+                        <span>My Github</span>
+                    </a>
+                    <a href='https://www.linkedin.com/in/ajit-vishwa-karma/' target='_blank'>
+                        <FaLinkedin className='text-3xl' />
+                        <span>My Linkedin</span>
+                    </a>
+                    <a className='hover:*:no-underline'>
+                        <SiGmail className='text-3xl' />
+                        <span>ajitvishwakarma202006@gmail.com</span>
+                    </a>
                 </div>
-                <div className='flex flex-col md:flex-row w-1/2 h-24 items-center justify-center gap-x-3 md:gap-x-6 hover:[&>a]:underline'>
-                    <FaLinkedin className='w-24 h-full transition-colors'/>
-                    <a className='text-white text-2xl md:text-4xl' target='_blank' href='https://linkedin.com/in/ajit-vishwa-karma/'>My LinkedIn</a>
-                </div>
-                <div className='flex flex-col md:flex-row w-1/2 h-24 items-center justify-center gap-x-3 md:gap-x-6'>
-                    <SiGmail className='w-24 h-full transition-colors'/>
-                    <a className='text-white md:text-2xl' target='_blank' >ajitvishwakarma202006@gmail.com</a>
-                </div>
+                <hr className='md:hidden w-full' />
+                <form className='w-4/5 md:w-2/5 h-full flex flex-col items-center justify-center gap-y-2 text-xl' action=''>
+                    <h3 className='text-2xl font-bold mb-2'>Message me here :</h3>
+                    <label htmlFor='name'>Your Name :</label>
+                    <input className='bg-slate-500 py-1 px-1 rounded-lg w-4/5' type='text' id='name' placeholder='Enter your name...' />
+                    <label htmlFor='e-mail'>Your Email :</label>
+                    <input className='bg-slate-500 py-1 px-1 rounded-lg w-4/5' type='email' id='e-mail' placeholder='Enter your email...' />
+                    <label htmlFor='message'>Your message :</label>
+                    <textarea className='resize-none bg-slate-500 py-1 px-1 rounded-lg w-4/5' id='message' placeholder='Enter your message...'></textarea>
+                    <input className='bg-slate-700 hover:bg-slate-600 active:bg-slate-900 py-1 px-1.5 rounded-full' type='submit' />
+                </form>
             </div>
         </div>
     )
