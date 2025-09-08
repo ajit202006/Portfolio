54f002fc78b2a52339d33b97e824e0ef48316091
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-08 18:59:01 +0530

    Changed navbar to be on top of the page rather than being aside

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,36 +1,23 @@
 import { useState } from 'react'
 import { TiThMenu } from 'react-icons/ti'
-import { LuBrainCircuit } from 'react-icons/lu'
-import { IoClose, IoPerson, IoSchool } from 'react-icons/io5'
-import { GoGoal } from "react-icons/go";
-import { HiOutlineMail } from "react-icons/hi";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className='fixed w-64 h-64 flex items-center rounded-l-full *:transition-all top-[calc(50%-8rem)] -right-32 z-50'>
-            <button
-                className='absolute right-1/2 p-3 rounded-l-full shadow-[0px_0px_4px_4px_rgba(255,255,255,0.25)] hover:scale-100'
-                onClick={() => setIsOpen(!isOpen)}>
-                {isOpen ? <IoClose /> : <TiThMenu />}
-            </button>
-            <a href='#about' title='About' className={`delay-50 ${isOpen ? 'rotate-70 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`} onClick={() => setIsOpen(false)}>
-                <IoPerson className='-rotate-70' />
-            </a>
-            <a href='#skills' title='Skills' onClick={() => setIsOpen(false)} className={`delay-25 ${isOpen ? 'rotate-35 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
-                <LuBrainCircuit className='-rotate-35' />
-            </a>
-            <a href='#education' title='Education' onClick={() => setIsOpen(false)} className={`${isOpen ? 'translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
-                <IoSchool/>
-            </a>
-            <a href='#projects' title='Projects' onClick={() => setIsOpen(false)} className={`delay-25 ${isOpen ? '-rotate-35 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
-                <GoGoal className='rotate-35' />
-            </a>
-            <a href='#contact' onClick={() => setIsOpen(false)} className={`delay-50 ${isOpen ? '-rotate-70 translate-x-3' : 'rotate-0 translate-x-36'} nav-btn`}>
-                <HiOutlineMail className='rotate-70'/>
-            </a>
+        <div className='fixed flex w-10 h-10 md:w-full md:h-16 bg-slate-900 justify-around items-center md:px-2 md:py-0.5  z-10'>
+            <TiThMenu className='md:hidden scale-100' onClick={() => {setIsOpen(!isOpen)}}/>
+            <div className='w-auto h-full hidden md:flex items-center gap-2.5'>
+                <img className='h-11/12 rounded-full' src='/profile.png' alt='Profile image' />
+                <h1>Ajit Vishwakarma</h1>
+            </div>
+            <ul className={`flex flex-col ${isOpen?'scale-100':'scale-0'} md:scale-100 transition-[scale] absolute left-15 *:w-20 *:bg-slate-900 *:text-center md:*:w-auto md:relative md:flex-row w-1/2 text-md lg:text-xl h-full justify-around items-center *:hover:bg-slate-800 md:*:py-1 md:*:px-6 md:*:rounded-full`}>
+                <li><a onClick={() => {setIsOpen(!isOpen)}} href='#about'>About</a></li>
+                <li><a onClick={() => {setIsOpen(!isOpen)}} href='#skills'>Skills</a></li>
+                <li><a onClick={() => {setIsOpen(!isOpen)}} href='#education'>Education</a></li>
+                <li><a onClick={() => {setIsOpen(!isOpen)}} href='#projects'>Projects</a></li>
+                <li><a onClick={() => {setIsOpen(!isOpen)}} href='#contact'>Contact</a></li>
+            </ul>
         </div>
-
     )
 }
 
