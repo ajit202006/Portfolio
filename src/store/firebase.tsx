2fa01a889109bb92318de861d678760198effe4e
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-09-18 19:42:49 +0530

    Created and added firebase application

--- src/store/firebase.tsx
+++ src/store/firebase.tsx
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'EY',
-  authDomain: 'portfolio-ajit-vishwakarma.firebaseapp.com',
-  projectId: 'portfolio-ajit-vishwakarma',
-  storageBucket: 'portfolio-ajit-vishwakarma.firebasestorage.app',
-  messagingSenderId: '850037257891',
-  appId: '1:850037257891:web:01d33d21e008df036a7116',
-  databaseURL:'https://portfolio-ajit-vishwakarma-default-rtdb.firebaseio.com'
+  apiKey: process.env.API_KEY,
+  authDomain: "portfolio-ajit-vishwakarma.firebaseapp.com",
+  databaseURL: "https://portfolio-ajit-vishwakarma-default-rtdb.firebaseio.com",
+  projectId: "portfolio-ajit-vishwakarma",
+  storageBucket: "portfolio-ajit-vishwakarma.firebasestorage.app",
+  messagingSenderId: "850037257891",
+  appId: "1:850037257891:web:bb487faf2f255e256a7116"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
